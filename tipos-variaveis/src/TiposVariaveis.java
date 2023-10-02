bdc5778a1ae31088e0afcdaa03a846f5c666acb4
Gustavo S Ribeiro <gustavo.ribeiro19@unifesp.br>
2023-10-02 17:25:33 -0300

    novos exemplos de tipos e variaveis, e criação de exemplos para operadors

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo");
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // Casting
+
+        int numero = 1;
+        numero = 2;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
