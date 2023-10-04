867f3fd5f19fd0af743db94e00a9be0567088e67
Gustavo S Ribeiro <gustavo.ribeiro19@unifesp.br>
2023-10-04 13:13:07 -0300

    novos exemplos de operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,22 +1,44 @@
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
+        int a, b, c, d;
 
-        numero = - numero;
-        System.out.println(numero);
-        numero = - numero;
-        System.out.println(numero);
+        a = 5;
+        b = 6;
 
-        numero = numero +1; // incremento
-        System.out.println(numero);
-        numero++; //incremento
-        System.out.println(numero);
-        System.out.println(++numero);//incremento e impressao
+        String resultado = "";
+        if (a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
 
-        numero+=2;
-        System.out.println(numero);
-        System.out.println(numero+=2);
-        System.out.println(numero);
-        System.out.println(numero/=2);
+        System.out.println(resultado);
+
+        c = 4;
+        d = 4;
+
+        String resultado2 = c==d ? "verdadeiro" : "falso";
+
+        System.out.println(resultado2);
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual a numerodois? "+ simNao);
+
+        String nome1 = "GUSTAVO";
+        String nome2 = new String("GUSTAVO");
+
+        System.out.println(nome1.equals(nome2));
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2)
+            System.out.println("as duas condições são verdadeiras");
+
+        if(condicao1 || condicao2)
+            System.out.println("uma condição é verdadeira");
     }
 }
